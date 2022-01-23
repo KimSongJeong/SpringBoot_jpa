a670912480ec509b619572b7b0cd17b8e883e6f7
KimSongJeong <rlathdwjd123@naver.com>
2022-01-23 13:38:07 +0900

    rest api 전까지 실습

--- src/main/java/com/song/blog/Entity/Board.java
+++ src/main/java/com/song/blog/Entity/Board.java
@@ -5,20 +5,18 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity // 이걸 붙여줘야 DB가 해당 객체를 인식할 수 있음
+@Entity // 이걸 붙여줘야 DB가 해당 객체를 인식할 수 있음 (해당 클래스로 테이블을 만듬)
 @Getter
 public class Board {
 
     @Id
-    @GeneratedValue // 자동생성 1, 2,3 .....
+//    @GeneratedValue // 자동생성 1, 2,3 .....
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 이렇게 해주면 DB가 알아서 id를 자동 생성! 1,2,3,4... 같긴한데 알아서 다음번호 다음번호 해줌
     private Long id;
 
     @Column
